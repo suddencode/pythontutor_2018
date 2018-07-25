158cbeede8863796d79a99e9606babed6f053ab2
Max <s3tis@ya.ru>
2018-07-25 13:20:53 +0300

    Задача «Следующее и предыдущее»
    Условие
    Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!).

---
+++ 1.6.py
@@ -0,0 +1,5 @@
+a = int(input())
+a1, a2 = a + 1, a - 1
+b1, b2 = str(a1), str(a2)
+print('The next number for the number', a, 'is '+ b1 +'.')
+print('The previous number for the number', a, 'is '+ b2 +'.')
