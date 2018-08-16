34d98a493f0d5267acf0fcf7ee4c79b158bd39d2
Max <s3tis@ya.ru>
2018-08-16 20:54:49 +0300

    Задача «Разность времен»
    Условие
    
    Даны значения двух моментов времени, принадлежащих одним и тем же суткам: часы, минуты и секунды для каждого из моментов времени. Известно, что второй момент времени наступил не раньше первого. Определите, сколько секунд прошло между двумя моментами времени.
    
    Программа на вход получает три целых числа: часы, минуты, секунды, задающие первый момент времени и три целых числа, задающих второй момент времени.
    
    Выведите число секунд между этими моментами времени.

---
+++ 3.8.py
@@ -0,0 +1,4 @@
+h1, m1, s1, h2, m2, s2 = int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
+time1 = (h1 * 60 * 60) + (m1*60) + s1
+time2 = (h2 * 60 * 60) + (m2*60) + s2
+print(time2-time1)
