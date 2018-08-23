471c7edfd4494675f078c9dbb076183ec8a297a4
Max <s3tis@ya.ru>
2018-08-23 19:19:16 +0300

    Задача «Сумма десяти чисел»
    Условие
    Дано 10 целых чисел. Вычислите их сумму. Напишите программу, использующую наименьшее число переменных.

---
+++ 4.4.py
@@ -0,0 +1,2 @@
+n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, = int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
+
